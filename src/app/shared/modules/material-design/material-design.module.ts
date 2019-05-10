ee5998947b7335fa05af6c8a0caeec9794d41bc4
Ron Badur <ronbadur02@gmail.com>
2019-05-11 01:33:26 +0300

    add table to my items

--- src/app/shared/modules/material-design/material-design.module.ts
+++ src/app/shared/modules/material-design/material-design.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
 
 const MatertialComponents = [
   MatButtonModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatTableModule
 ];
   
 @NgModule({
