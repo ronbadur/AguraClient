1fa999fe661f26fef4608f1fb1ee347ac71c360f
Ron Badur <ronbadur02@gmail.com>
2019-05-11 15:24:32 +0300

    implemet table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './shared/modules/routing/app-routing.module';
 import { AppComponent } from './app.component';
-import { MyItemsComponent } from './my-items/my-items.component';
-import { GiveSomethingComponent } from './give-something/give-something.component';
-import { TakeSomethingComponent } from './take-something/take-something.component';
-import { StatisticsComponent } from './statistics/statistics.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: '', component: MyItemsComponent },
-  { path: 'my-items', component: MyItemsComponent },
-  { path: 'give-something', component: GiveSomethingComponent },
-  { path: 'take-something', component: TakeSomethingComponent },
-  { path: 'statistics', component: StatisticsComponent },
-  { path: '**', component: PageNotFoundComponent }
-];
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesignModule } from './shared/modules/material-design/material-design.module';
+import { ItemService } from './shared/services/item/item.service';
+import { ItemsMock } from './shared/data/ItemsMock';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyItemsComponent,
-    GiveSomethingComponent,
-    TakeSomethingComponent,
-    StatisticsComponent,
-    PageNotFoundComponent,
+    AppComponent
   ],
   imports: [
-    RouterModule.forRoot(
-      appRoutes
-    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule
   ],
-  providers: [],
+  providers: [ItemService, ItemsMock],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/services/item/item.service.ts
+++ src/app/shared/services/item/item.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { ItemsMock } from '../../data/ItemsMock';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  constructor(private mockData: ItemsMock) { }
+
+  fetchItemsByUsername(username: string) {
+      return this.mockData.data;
+  }
+
+  fetchAllItems() {
+    return null;
+  }
 }
