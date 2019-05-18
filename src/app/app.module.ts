63ad7179e36db4b9178754f14835a802f90ff58b
Bar Cohen <barofi14@gmail.com>
2019-05-18 17:55:31 +0300

    Add pie chart componenet and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,21 @@ import { MaterialDesignModule } from './shared/modules/material-design/material-
 import { ReactiveFormsModule } from '@angular/forms';
 import { ItemService } from './shared/services/item/item.service';
 import { ItemsMock } from './shared/data/ItemsMock';
+import { FooterComponent } from './footer/footer.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialDesignModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [ItemService, ItemsMock],
   bootstrap: [AppComponent]
