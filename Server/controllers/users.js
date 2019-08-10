9cc4f6e026aeec4091acda0053965f7135087393
rotem bar <rotem4432@gmail.com>
2019-08-10 17:31:18 +0300

    update user controller

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -1,22 +1,65 @@
 const express = require('express');
 const router = express.Router();
-
+const user = require('../models/user');
 
 // GET HTTP method to /users
 router.get('/',(req,res) => {
-    res.send([{
-        "name": "Bar"
-    },
-{
-    "name": "Ron"
-}]);
+    user.getAllUsers((err, users) => {
+        if(err) {
+            res.json({success:false, message: `Failed to load all users. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({ success: true, users: users }, null, 2));
+            res.end();
+        }
+    });
 });
 
 // GET HTTP method to /users/:mail/:password
 router.get('/:mail/:password',(req,res) => {
-    res.send([{
-        "name": "Bar"
-    }]);
+    user.getUserByMailAndPassword(req.params.mail, req.params.password,(err, user)=> {
+        if(err) {
+            res.json({success:false, message: `Failed to find user. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({ success: true, user: user }, null, 2));
+            res.end();
+        }
+    });
 });
+
+// GET HTTP method to /users/:id
+router.get('/:id',(req,res) => {
+    user.getUserByID(req.params.id,(err, user)=> {
+        if(err) {
+            res.json({success:false, message: `Failed to find user by ID. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({ success: true, user: user }, null, 2));
+            res.end();
+        }
+    });
+});
+
+//POST HTTP method to /users
+router.post('/', (req,res,next) => {
+    let newUser = {
+        password: req.body.password,
+        fullName: req.body.fullName,
+        mail: req.body.mail,
+        phone: req.body.phone,
+        city: req.body.city,
+        admin: req.body.admin
+    };
+    
+    user.editUser(newUser,(err, newUser) => {
+        if(err) {
+            res.json({success: false, message: `Failed to create a new user. Error: ${err}. req: ${req}`});
+        } else {
+            res.json({success:true, message: `Added successfully: ${newUser}`});
+        }
+    });
+});
+
 
 module.exports = router;
