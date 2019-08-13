a07c1d66daa7ba604f79c2f25a00b048e8235858
Bar Cohen <barofi14@gmail.com>
2019-08-13 20:40:27 +0300

    add weather api

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {SideNavStatusService} from './shared/services/side-nav-status/side-nav-status.service';
 import {Router} from '@angular/router';
 import {UserService} from './shared/services/user/user.service';
+import { stringify } from '@angular/compiler/src/util';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +12,19 @@ import {UserService} from './shared/services/user/user.service';
 })
 export class AppComponent implements OnInit {
   title = 'agura';
+  weather : string;
 
-  constructor(private router: Router, private sideNavStatusService: SideNavStatusService, private userLoggedInStatus: UserService) {
+  constructor(private router: Router, private sideNavStatusService: SideNavStatusService, private userLoggedInStatus: UserService,private http: HttpClient) {
   }
 
   ngOnInit() {
-  }
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=jerusalem&APPID=65252e392715d7598a0c9c5ca14e4243&units=metric";
+  console.log("im here");
+  this.http.get(url).subscribe((data) => {
+      this.weather= (data as any).main.temp;
+      
+  });
 
-  logout() {
-    this.userLoggedInStatus.changeUserLoggedInStatus();
-    this.router.navigate(['']);
+
   }
 }
